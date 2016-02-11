cf262a7c51cf1d52d7fc967ad91a03a59ffede9e
Rian Stockbower <rstockbower@gmail.com>
2016-02-10 21:44:30 -0500

    Backward iterating loop

--- Countdown/src/Countdown.scala
+++ Countdown/src/Countdown.scala
@@ -7,7 +7,14 @@ object Countdown {
     }
   }
 
+  def CountdownLoop(n: Int) : Unit = {
+    for (i <- n to 0 by -1) {
+      println(i)
+    }
+  }
+
   def main(args: Array[String]) = {
     CountdownRecursive(5)
+    CountdownLoop(3)
   }
 }
